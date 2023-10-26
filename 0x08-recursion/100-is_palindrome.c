12ee24ad53f933878d497441ca9e068865fd3341
cusink859 <gagyapong@servaco.com.gh>
2023-10-25 22:53:17 -0700

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,11 +9,12 @@ int _strlen_recursion(char *s);
  *
  * Return: 1 if it is, 0 it's not
  */
+
 int is_palindrome(char *s)
 {
-        if (*s == 0)
-                return (1);
-        return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -24,9 +25,9 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -39,9 +40,10 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-        if (*(s + i) != *(s + len - 1))
-                return (0);
-        if (i >= len)
-                return (1);
-        return (check_pal(s, i + 1, len - 1));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
+
