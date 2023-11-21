15616aae2a127faf70929dff5572ef6226beb310
cusink859 <gagyapong@servaco.com.gh>
2023-11-21 01:55:02 -0800

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
- size_t num = 0;
+	size_t nodes = 0;
 
- while (h)
- {
- num++;
- h = h->next;
- }
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
 
- return (num);
+	return (nodes);
 }
